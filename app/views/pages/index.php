701ef5eb3619e798c923fecd83c04cc32171e6f5
Naveler <georg.vilja@khk.ee>
2021-05-26 09:15:47 +0300

    added and configured framework files

---
+++ app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php require_once APPROOT.'/views/inc/header.php'; ?>
+<h1><?php echo $data['title'];?></h1>
+<p><?php echo $data['content'][0];?>
+
+</p>
+<?php require_once APPROOT.'/views/inc/footer.php'; ?>
