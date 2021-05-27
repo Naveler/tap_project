48867f21a6a68afc2d9802e5abce24e56026a4eb
Naveler <georg.vilja@khk.ee>
2021-05-27 15:41:18 +0300

    navbar orientation

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
     <div class="container">
-        <a href="<?php echo URLROOT; ?>" class="navbar-brand"><?php echo PROJECT_NAME; ?></a>
+        <a href="<?php echo URLROOT; ?>/partners/index" class="navbar-brand"><?php echo PROJECT_NAME; ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a href="<?php echo URLROOT; ?>" class="nav-link">Home</a>
+                    <a href="<?php echo URLROOT; ?>/partners/index" class="nav-link">Koostööpartnerid</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo URLROOT; ?>/activities/index" class="nav-link">Teenused</a>
                 </li>
             </ul>
         </div>
